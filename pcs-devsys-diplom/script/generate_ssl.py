e5155cf2b372293ea084c2ffc6bff7dfa5c27977
Ozolin <dmozolin123@gmail.com>
2022-01-22 15:50:22 +0500

    fix script bash and python

--- pcs-devsys-diplom/script/generate_ssl.py
+++ pcs-devsys-diplom/script/generate_ssl.py
@@ -1,19 +1,21 @@
 import requests
 import os 
+login_vault = os.getenv("login_vault") # export login_vault=123123123
 a = requests.post("http://127.0.0.1:8201/v1/pki_int/issue/example-dot-com",
                     headers={"X-Vault-Token": "s.5Pi5yMABH0ZJMNokDMRZ9X9t"}, 
                     data={"common_name": "test.dozolin.devops.ru", "ttl": "720h"})
 data = a.json()
-with open("/etc/cert_script/ca_chain.pem", "w") as ca_chain:
-    ca_chain.write(data["data"]["ca_chain"][0])
+if data:
+    with open("/etc/cert_script/ca_chain.pem", "w") as ca_chain:
+        ca_chain.write(data["data"]["ca_chain"][0])
 
-with open("/etc/cert_script/certificate.pem", "w") as certificate:
-    certificate.write(data['data']["certificate"])
+    with open("/etc/cert_script/certificate.pem", "w") as certificate:
+        certificate.write(data['data']["certificate"])
 
-with open("/etc/cert_script/issuing_ca.pem", "w") as issuing_ca:
-    issuing_ca.write(data['data']["issuing_ca"])
+    with open("/etc/cert_script/issuing_ca.pem", "w") as issuing_ca:
+        issuing_ca.write(data['data']["issuing_ca"])
 
-with open("/etc/cert_script/private_key.pem", "w") as private_key:
-    private_key.write(data['data']["private_key"])
+    with open("/etc/cert_script/private_key.pem", "w") as private_key:
+        private_key.write(data['data']["private_key"])
 
-os.popen("systemctl reload nginx")
+    os.popen("systemctl reload nginx")
--- pcs-devsys-diplom/script/generate_ssl.sh
+++ pcs-devsys-diplom/script/generate_ssl.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
-vault login s.5Pi5yMABH0ZJMNokDMRZ9X9t
+vault login $login_vault # export login_vault=123123123
 
 vault write pki_int/issue/example-dot-com common_name="test.dozolin.devops.ru" ttl="720h" -format=json > /etc/cert_script/data.json
 
-cat /etc/cert_script/data.json | jq -r '.data.ca_chain[0]' > /etc/cert_script/ca_chain.pem
-cat /etc/cert_script/data.json | jq -r '.data.certificate' > /etc/cert_script/certificate.pem
-cat /etc/cert_script/data.json | jq -r '.data.issuing_ca' > /etc/cert_script/issuing_ca.pem
-cat /etc/cert_script/data.json | jq -r '.data.private_key' > /etc/cert_script/private_key.pem
+if [[ -s /etc/cert_script/data.json ]]
+then 
+    cat /etc/cert_script/data.json | jq -r '.data.ca_chain[0]' > /etc/cert_script/ca_chain.pem
+    cat /etc/cert_script/data.json | jq -r '.data.certificate' > /etc/cert_script/certificate.pem
+    cat /etc/cert_script/data.json | jq -r '.data.issuing_ca' > /etc/cert_script/issuing_ca.pem
+    cat /etc/cert_script/data.json | jq -r '.data.private_key' > /etc/cert_script/private_key.pem
 
-rm /etc/cert_script/data.json
+    rm /etc/cert_script/data.json
 
-systemctl reload nginx
+    systemctl reload nginx
+else
+    rm /etc/cert_script/data.json
+    echo "Generation problem"
+if
